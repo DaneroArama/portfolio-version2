78fa13f1fda2b0670367786b0ffd651d5ac58375
DaneroArama <www.aviciiguetta@gmail.com>
2025-02-03 18:15:24 +0630

    Commit

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -1,9 +1,17 @@
-import {Previews} from '@react-buddy/ide-toolbox'
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
+import About from "../components/About";
+import Services from "../components/Services";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/About">
+                <About/>
+            </ComponentPreview>
+            <ComponentPreview path="/Services">
+                <Services/>
+            </ComponentPreview>
         </Previews>
     )
 }
